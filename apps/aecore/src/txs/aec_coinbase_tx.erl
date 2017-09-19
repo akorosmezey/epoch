f47dc332a4107e911fea566af3b147b844cabc32
Akos Korosmezey <akos.korosmezey@erlang-solutions.com>
2017-09-19 17:56:12 +0200

    GH-111 Coinbase trx with account creation

--- apps/aecore/src/txs/aec_coinbase_tx.erl
+++ apps/aecore/src/txs/aec_coinbase_tx.erl
@@ -1,7 +1,9 @@
 -module(aec_coinbase_tx).
 
 %% API
--export([new/2, run/3]).
+-export([new/2,
+         check/3,
+         process/3]).
 
 -behavior(aec_tx).
 
@@ -15,8 +17,22 @@
 new(#{account := AccountPubkey}, _Trees) ->
     {ok, #coinbase_tx{account = AccountPubkey}}.
 
--spec run(coinbase_tx(), trees(), non_neg_integer()) -> {ok, trees()} | {error, term()}.
-run(#coinbase_tx{account = AccountPubkey}, Trees0, Height) ->
+-spec check(coinbase_tx(), trees(), non_neg_integer()) -> {ok, trees()} | {error, term()}.
+check(#coinbase_tx{account = AccountPubkey}, Trees0, Height) ->
+    AccountsTrees0 = aec_trees:accounts(Trees0),
+    case aec_accounts:get(AccountPubkey, AccountsTrees0) of
+        {ok, _Account} ->
+            {ok, Trees0};
+        {error, notfound} ->
+            %% Add newly referenced account (w/0 amount) to the state
+            Account = aec_accounts:new(AccountPubkey, 0, Height),
+            {ok, AccountsTrees} = aec_accounts:put(Account, AccountsTrees0),
+            Trees = aec_trees:set_accounts(Trees0, AccountsTrees),
+            {ok, Trees}
+    end.
+
+-spec process(coinbase_tx(), trees(), non_neg_integer()) -> {ok, trees()} | {error, term()}.
+process(#coinbase_tx{account = AccountPubkey}, Trees0, Height) ->
     AccountsTrees0 = aec_trees:accounts(Trees0),
     case aec_accounts:get(AccountPubkey, AccountsTrees0) of
         {ok, Account0} ->
